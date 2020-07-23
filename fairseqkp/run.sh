dcc801484d9b23951970befbc9b70783224c4edc
wasiahmad <wasiahmad@ucla.edu>
2020-07-23 02:04:22 -0500

    new update

--- fairseqkp/run.sh
+++ fairseqkp/run.sh
@@ -3,7 +3,38 @@
 SRCDIR=data
 mkdir -p logs
 
-function train () {
+function rnn_train () {
+
+export CUDA_VISIBLE_DEVICES=$1
+DATASET=$2
+TOTAL_NUM_UPDATES=50000
+ARCH=lstm
+
+# https://github.com/pytorch/fairseq/blob/ffecb4e3496379edf5ecae1483df5b7e0886c264/fairseq/models/transformer.py#L902
+fairseq-train ${SRCDIR}/${DATASET}-bin/ \
+--fp16 --num-workers 4 --save-dir ${DATASET}_checkpoints \
+--skip-invalid-size-inputs-valid-test \
+--arch $ARCH --task translation \
+--max-tokens 8192 --truncate-source \
+--max-source-positions 512 --max-target-positions 512 \
+--encoder-embed-dim 512 --decoder-embed-dim 512 \
+--source-lang source --target-lang target \
+--encoder-layers 2 --decoder-layers 2 --encoder-bidirectional \
+--encoder-hidden-size 512 --decoder-hidden-size 512 \
+--decoder-attention 1 --dropout 0.2 \
+--share-all-embeddings --share-decoder-input-output-embed \
+--required-batch-size-multiple 1 \
+--criterion label_smoothed_cross_entropy --label-smoothing 0.1 \
+--weight-decay 0.01 --optimizer adam --adam-betas "(0.9, 0.999)" --adam-eps 1e-08 \
+--clip-norm 0.1 --lr-scheduler polynomial_decay --lr 1e-04 \
+--max-update $TOTAL_NUM_UPDATES --max-epoch 25 --update-freq 1 \
+--validate-interval 1 --patience 3 --no-epoch-checkpoints \
+--find-unused-parameters --ddp-backend=no_c10d \
+--log-format=json 2>&1 | tee logs/rnn.log
+
+}
+
+function transformer_train () {
 
 export CUDA_VISIBLE_DEVICES=$1
 DATASET=$2
@@ -32,7 +63,7 @@ fairseq-train ${SRCDIR}/${DATASET}-bin/ \
 --max-epoch 25 --update-freq 2 \
 --validate-interval 1 --patience 3 --no-epoch-checkpoints \
 --find-unused-parameters --ddp-backend=no_c10d \
---log-format=json 2>&1 | tee logs/train.log
+--log-format=json 2>&1 | tee logs/transformer.log
 
 }
 
@@ -41,13 +72,14 @@ function decode () {
 export CUDA_VISIBLE_DEVICES=$1
 DATASET=$2
 SAVE_DIR_PREFIX=$3
+OUTPUT_FILE=$4
 
 python decode.py \
 --data_name_or_path "$SRCDIR/${DATASET}-bin/" \
 --data_dir "$SRCDIR/${DATASET}/" \
 --checkpoint_dir ${SAVE_DIR_PREFIX}_checkpoints \
 --checkpoint_file checkpoint_best.pt \
---output_file logs/${DATASET}_test.hypo \
+--output_file $OUTPUT_FILE \
 --batch_size 64 \
 --beam 1 \
 --min_len 1 \
@@ -75,20 +107,21 @@ while getopts ":h" option; do
         echo "Syntax: run.sh GPU_ID DATASET_NAME"
         echo
         echo "GPU_ID         A list of gpu ids, separated by comma. e.g., '0,1,2'"
-        echo "DATASET_NAME   Name of the training dataset. e.g., kp20k, kptimes, etc."
+        echo "DATASET_NAME   Name of the training dataset. Choices: kp20k, kptimes."
+        echo "MODEL_TYPE     Model type. Choices: rnn, transformer."
         echo
         exit;;
    esac
 done
 
 if [[ $2 == 'kp20k' ]]; then
-    train "$1" $2
+    $3_train "$1" $2
     for dataset in kp20k inspec krapivin nus semeval; do
-        decode "$1" $dataset $2
-        evaluate ${SRCDIR}/${dataset} "logs/${dataset}_test.hypo" $dataset
+        decode "$1" $dataset $2 logs/${3}_${dataset}_test.hypo
+        evaluate ${SRCDIR}/${dataset} logs/${3}_${dataset}_test.hypo ${3}_${dataset}
     done
 elif [[ $2 == 'kptimes' ]]; then
-    train "$1" $2
-    decode "$1" $2 $2
-    evaluate ${SRCDIR}/${2} "logs/${2}_test.hypo" $2
+    $3_train "$1" $2
+    decode "$1" $2 $2 logs/${3}_${2}_test.hypo
+    evaluate ${SRCDIR}/${2} logs/${3}_${2}_test.hypo ${3}_${2}
 fi
